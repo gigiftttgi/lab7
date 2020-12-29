19b7cc708333ece87c4c02846883587842859172
gigiftttgi <natcha_si@cmu.ac.th>
2020-12-30 00:01:54 +0700

    lab7_2_final

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,27 +3,39 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float sc)
+{
+	char a='A',b='B',c='C',d='D',f='F';
+	if(sc>90)
+		return a;
+	else if(sc>75)
+		return b;
+	else if(sc>60)
+		return c;
+	else if(sc>45)
+		return d;
+	else
+		return f;	
+}
 
-int main(){
-	//Input the number of students
+int main()
+{
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
 	float score[N];	
 	
-	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
-	
-	//Print names scores and grades
+
+
 	i = 0;
 	cout << "---------------------------------------------\n";
 	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
